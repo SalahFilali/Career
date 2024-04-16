7269ebd410fbb6023fb6fd3f6d840f8f36cef141
Salah FILALI <salahfilali75@gmail.com>
2024-04-16 15:06:17 +0200

    Init project

---
+++ Career/Career/CareerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CareerApp.swift
+//  Career
+//
+//  Created by FILALI Salah on 16/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CareerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
